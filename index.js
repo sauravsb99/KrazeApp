84cc38eef9e7183526c3e87d7f0abe2ce235ca47
Nikhil Joji <nikki@192.168.1.5>
2022-09-26 02:15:04 +0530

    Added NodeGeoCoder

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const options = {
   provider: 'google',
 
   // Optional depending on the providers
-  apiKey: 'AIzaSyCGZZvzZjJ7fWSI4J82I89eJEKi4Oc8p0E', // for Mapquest, OpenCage, Google Premier
+  apiKey: "API_KEY", // for Mapquest, OpenCage, Google Premier
   formatter: null // 'gpx', 'string', ...
 };
 
@@ -16,23 +16,32 @@ var fs = require('fs');
 
 try {  
     var data = fs.readFileSync('input.txt', 'utf8');
-    console.log(data.toString());    
+    var data_list = data.split(/\r?\n/);
+    console.log(data_list);    
 } catch(e) {
     console.log('Error:', e.stack);
 }
 
-fs.writeFile('output.txt', data, function (err) {
+var result ="";
+for(var i = 0 ; i < data_list.length;i++){
+  geoCoder.geocode(data_list[i])
+  .then((res)=> {
+    result+=res;
+    console.log(res);
+  })
+  .catch((err)=> {
+    console.log(err);
+  });
+}
+
+
+
+fs.writeFile('output.txt', result, function (err) {
   if (err) return console.log(err);
   console.log('Data > output.txt');
 });
 
-// geoCoder.geocode('Location')
-//   .then((res)=> {
-//     console.log(res);
-//   })
-//   .catch((err)=> {
-//     console.log(err);
-//   });
+
 
   
 ;
