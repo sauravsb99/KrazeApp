2b596ffd6bc71514706ea60b9affb5a4ab1b9b94
Nikhil Joji <nikki@192.168.1.5>
2022-09-26 12:48:27 +0530

    Need to add cache

--- index.js
+++ index.js
@@ -1,47 +1,82 @@
 const NodeGeocoder = require('node-geocoder');
+const fs = require('fs');
 
 const options = {
   provider: 'google',
 
-  // Optional depending on the providers
   apiKey: "API_KEY", // for Mapquest, OpenCage, Google Premier
   formatter: null // 'gpx', 'string', ...
 };
 
 const geoCoder = NodeGeocoder(options);
 
-
-
-var fs = require('fs');
-
-try {  
-    var data = fs.readFileSync('input.txt', 'utf8');
-    var data_list = data.split(/\r?\n/);
-    console.log(data_list);    
-} catch(e) {
-    console.log('Error:', e.stack);
-}
-
-var result ="";
-for(var i = 0 ; i < data_list.length;i++){
-  geoCoder.geocode(data_list[i])
-  .then((res)=> {
-    result+=res;
-    console.log(res);
-  })
-  .catch((err)=> {
-    console.log(err);
+function readInputFile() {
+  return new Promise(resolve => {
+    var filename = process.argv[2];
+    fs.readFile(filename, 'utf8', function(err, data) {
+    if (err) throw err;
+    console.log('OK: ' + filename);
+    data_list = data.split(/\r?\n/);
+    resolve(data_list);
+    });
   });
 }
 
+function getLatLong(arg) {
+  return new Promise(resolve => {
+    resolve("lat 25 long 36");
+    // geoCoder.geocode(arg)
+    // .then((res)=> {
+    //   resolve(res);
+    //   console.log(res);
+    // })
+    // .catch((err)=> {
+    //   console.log(err);
+    //   throw err;
+    // });
+    });
+}
+
+function writeOutputFile(result) {
+  return new Promise(resolve => {
+    fs.writeFile('output.txt', result, function (err) {
+      if (err) return console.log(err);
+      console.log('Data > output.txt');
+      resolve("OK");
+      });
+  });
+}
+
+async function getGeoCodes(){
+  if (process.argv.length < 3) {
+    console.log('Usage: node ' + process.argv[1] + ' FILENAME');
+    process.exit(1);
+  }
+  try{
+    const data_list = await readInputFile(); 
+  }
+  catch{
+    console.log("Exception Caught while reading the input File");
+  }
+  var result = "";
+  for(let i = 0 ; i < data_list.length;i++){
+    try{
+      result += await getLatLong(data_list[i]);
+      result+="\n";
+    }
+    catch{
+      console.log("Exception Caught while converting data into geocodes");
+    }
+  }
+
+  const isWriteOK = await writeOutputFile(result);
+
+  console.log(isWriteOK);
 
 
-fs.writeFile('output.txt', result, function (err) {
-  if (err) return console.log(err);
-  console.log('Data > output.txt');
-});
+}
 
-
+getGeoCodes();
 
   
 ;
